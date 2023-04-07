a48b51e317ef0b139397440c9ace5c79395f3e36
Stuart Sumner <stuartsumner@Stuarts-MacBook-Pro.local>
2023-04-07 16:09:01 -0500

    Data wrangling

---
+++ 0_data_tidying.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+life <- read_csv("/Users/stuartsumner/Desktop/Senior Year/DataSci Spring/Final/data/life_expectancy_initial.csv")
+skimr::skim(life)
+
+colnames <- c("Country", "Following_year", "Following_life_expect")
+
+following_year <- life %>% 
+  group_by(Country, Year) %>% 
+  select(Country, Year, Life_expectancy)
+colnames(following_year) <- colnames
+
+following_year$Year <- following_year$Following_year - 1
+
+life %>% 
+  arrange(desc(Year))
+
+x <- full_join(life, following_year, c("Country", "Year"))
+
+x %>% 
+  filter(Country == "Jordan") %>% 
+  select(1, 2, 3, "Life_expectancy", 23) %>% 
+  arrange(desc(Year))
+
+view(x)
+
+x %>% 
+  filter(Country == "Jordan") %>% 
+  view()
