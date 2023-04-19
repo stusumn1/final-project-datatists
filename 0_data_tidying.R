d759fb07112c3e8f7a0aa11cbbbf913b74f0f2a9
Stuart Sumner <stuartsumner@Stuarts-MacBook-Pro.local>
2023-04-19 00:08:44 -0500

    Data cleaning, initial setup

--- 0_data_tidying.R
+++ 0_data_tidying.R
@@ -1,29 +1,60 @@
+set.seed(4444)
+
 library(tidyverse)
+library(tidymodels)
+tidymodels_prefer()
 
-life <- read_csv("data/raw/life_expectancy_initial.csv")
-skimr::skim(life)
+## data source: https://www.kaggle.com/datasets/lashagoch/life-expectancy-who-updated
+life_expec <- read_csv("data/raw/life_expectancy_initial.csv")
+skimr::skim(life_expec)
 
+## create current and previous year data by country, by year
 colnames <- c("Country", "Following_year", "Following_life_expect")
 
-following_year <- life %>% 
+following_year <- life_expec %>% 
   group_by(Country, Year) %>% 
   select(Country, Year, Life_expectancy)
 colnames(following_year) <- colnames
 
 following_year$Year <- following_year$Following_year - 1
 
-life %>% 
-  arrange(desc(Year))
+life <- full_join(life_expec, following_year, c("Country", "Year")) %>% 
+  janitor::clean_names() %>% 
+  mutate(
+    region = factor(region),
+    developed = factor(economy_status_developed, levels = c(1, 0))
+  )
 
-x <- full_join(life, following_year, c("Country", "Year"))
+## split data using 2015 data as test
+life_test <- life %>% 
+  filter(year == 2014) %>% 
+  select(-1, -3, -19, -20, -22)
 
-x %>% 
-  filter(Country == "Jordan") %>% 
-  select(1, 2, 3, "Life_expectancy", 23) %>% 
-  arrange(desc(Year))
+life_train <-  life %>% 
+  filter(year != 2014 & year != 2015) %>% 
+  select(-1, -3, -19, -20, -22)
 
-view(x)
+## create folds
+life_folds <- vfold_cv(life_train, v = 10, repeats = 3)
 
-x %>% 
-  filter(Country == "Jordan") %>% 
-  view()
+## create initial recipe
+basic_recipe <- recipe(following_life_expect ~ ., data = life_train) %>% 
+  step_rm(thinness_ten_nineteen_years) %>% 
+  step_dummy(region, developed) %>% 
+  step_normalize(all_numeric_predictors()) %>% 
+  prep()
+
+bake(basic_recipe, new_data = NULL)
+
+## save some initial setup
+keep_pred <- control_grid(save_pred = T, save_workflow = T)
+life_metrics <- yardstick::metric_set(rmse,
+                                      mae,
+                                      rsq)
+
+save(life_test, 
+     life_train, 
+     life_folds, 
+     basic_recipe,
+     keep_pred, 
+     life_metrics, file = "initial_setup/tuning_setup.rda")
