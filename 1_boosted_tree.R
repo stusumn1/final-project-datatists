321bb562a7df3d37a73583b6b6c59d51e734791b
Nina Meng <ninameng@dhcp-10-105-13-188.wireless.northwestern.private>
2023-05-07 16:51:10 -0500

    bt fixes: didn't work :(

--- 1_boosted_tree.R
+++ 1_boosted_tree.R
@@ -30,7 +30,7 @@ hardhat::extract_parameter_set_dials(bt_spec)
 # setup tuning grid 
 bt_params <- hardhat::extract_parameter_set_dials(bt_spec) %>% 
   update(
-    mtry = mtry(range = c(1, 5)),
+    mtry = mtry(range = c(1, 10)),
     learn_rate = learn_rate(range = c(-5, -0.2))
   )
 
