af32f335a57f5b49ca21b6a46b4f4825aa36c2a8
Nina Meng <ninameng@dhcp-10-105-13-188.wireless.northwestern.private>
2023-05-31 18:58:42 -0500

    pt 2

---
+++ tables/recipe2_datatable.R
@@ -0,0 +1,69 @@
+library(tidymodels)
+library(tidyverse)
+
+
+# handle common conflicts 
+tidymodels_prefer()
+
+load("results/bt_tune_filter.rda")
+load("results/en_tune_filter.rda")
+load("results/knn_tune_filter.rda")
+load("results/mars_tune_filter.rda")
+load("results/rf_tune_filter.rda")
+load("results/svm_poly_tune_filter.rda")
+load("results/svm_radial_tune_filter.rda")
+load("results/nn_tune_filter.rda")
+
+load("initial_setup/tuning_setup.rda")
+
+
+model_set <- 
+  as_workflow_set( 
+    "elastic net" = en_tune,
+    "random forest" = rf_tune,
+    "k nearest neighbor" = knn_tune,
+    "boosted tree" = bt_tune,
+    "neural network" = nn_tune,
+    "svm polynomial" = svm_poly_tune,
+    "svm radial" = svm_radial_tune,
+    "mars" = mars_tune
+  )
+
+
+
+
+temp2 <- model_set %>% 
+  collect_metrics() %>% 
+  filter(.metric == "rmse") %>% 
+  slice_min(order_by = mean, by = wflow_id) %>% 
+  DT::datatable() 
+
+temp2
+# 
+# model_type <- c("elastic net",
+#                 "random forest",
+#                 "k nearest neighbor",
+#                 "boosted tree",
+#                 "neural network",
+#                 "svm polynomial",
+#                 "svm radial",
+#                 "mars")
+# 
+# times <- c("18.0",
+#            "247",
+#            "14.1",
+#            "79.0",
+#            "45.8",
+#            "54.0",
+#            "52.6",
+#            "5.42")
+# 
+# time_tibble <- bind_cols(model_type, times)
+# time_df <- as.data.frame(time_tibble)
+# 
+# names(time_df) = c("Model Type", "Run Time (seconds)")
+
+# time_df %>% 
+#   DT::datatable()
+
+save(temp2, file = "datatable2.rda")
